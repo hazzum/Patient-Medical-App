6cb671c5babe16a4df49ca932a723e0f2a24c25e
Elsam-Mohamed-Gharieb <islam67010@stemegypt.edu.eg>
2021-06-17 16:15:38 +0200

    Connecting back end

---
+++ lib/Data/json_data.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SearchService {
+  List doctorsJsonList = [];
+  getData() {
+    return FirebaseFirestore.instance
+        .collection('doctors')
+        .get().then((QuerySnapshot doc) {
+      for (int i = 0; i < doc.docs.length; ++i) {
+        doctorsJsonList.add(doc.docs[i].data());
+      }
+    });
+  }
+
+  returnData(){
+    getData();
+    return doctorsJsonList;
+  }
+
+}
